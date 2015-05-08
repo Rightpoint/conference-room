606c42a2158be33002e84674acf307937d1ed9d5
Jonathan Rupp <jorupp@gmail.com>
2015-05-08 16:29:58 -0500

    Quick hackjob of possibilities

--- RightpointLabs.ConferenceRoom.Services/App_Start/WebApiConfig.cs
+++ RightpointLabs.ConferenceRoom.Services/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace RightpointLabs.ConferenceRoom.Services
@@ -10,6 +11,8 @@ namespace RightpointLabs.ConferenceRoom.Services
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +22,7 @@ namespace RightpointLabs.ConferenceRoom.Services
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
         }
     }
 }
---
+++ RightpointLabs.ConferenceRoom.Services/Controllers/RoomController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace RightpointLabs.ConferenceRoom.Services.Controllers
+{
+    public class RoomController : ApiController
+    {
+        private static ExchangeService GetService()
+        {
+            var svc = new ExchangeService(ExchangeVersion.Exchange2010);
+            svc.Credentials = new WebCredentials(ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"]);
+            var serviceUrl = ConfigurationManager.AppSettings["serviceUrl"];
+            if (!string.IsNullOrEmpty(serviceUrl))
+            {
+                svc.Url = new Uri(serviceUrl);
+            }
+            else
+            {
+                svc.AutodiscoverUrl(ConfigurationManager.AppSettings["username"], url => new Uri(url).Scheme == "https");
+            }
+            return svc;
+        }
+
+        [HttpGet]
+        public object Lists()
+        {
+            var svc = GetService();
+            return svc.GetRoomLists().Select(i => new
+            {
+                i.Id,
+                i.Address,
+                i.MailboxType,
+                i.Name,
+                i.RoutingType
+            }).ToList();
+        }
+
+        [HttpGet]
+        public object Rooms(string address)
+        {
+            var svc = GetService();
+            return svc.GetRooms(address).Select(i => new
+            {
+                i.Id,
+                i.Address,
+                i.MailboxType,
+                i.Name,
+                i.RoutingType
+            }).ToList();
+        }
+
+        [HttpGet]
+        public object Schedule(string roomAddress)
+        {
+            var svc = GetService();
+            var calId = new FolderId(WellKnownFolderName.Calendar, new Mailbox(roomAddress));
+            var cal = CalendarFolder.Bind(svc, calId);
+            return cal.FindAppointments(new CalendarView(DateTime.Today, DateTime.Today.AddDays(1))).Select(i => new
+            {
+                i.Id,
+                i.Subject,
+                i.Start,
+                i.End,
+                Organizer = i.Organizer.Name,
+            }).ToList();
+        }
+    }
+}
