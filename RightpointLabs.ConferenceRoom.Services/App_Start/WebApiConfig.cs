215c02d3efccd99330c09ddafaf55c715d1108f4
Jonathan Rupp <jorupp@gmail.com>
2015-05-16 15:31:43 -0500

    Remove double l4n config

--- RightpointLabs.ConferenceRoom.Services/App_Start/WebApiConfig.cs
+++ RightpointLabs.ConferenceRoom.Services/App_Start/WebApiConfig.cs
@@ -10,8 +10,6 @@ namespace RightpointLabs.ConferenceRoom.Services
     {
         public static void Register(HttpConfiguration config)
         {
-            InitLogging();
-
             // Web API configuration and services
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             UnityConfig.RegisterComponents();
@@ -24,16 +22,6 @@ namespace RightpointLabs.ConferenceRoom.Services
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-        }
-
-        private static void InitLogging()
-        {
-            // initialize log4net
-            var file = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config");
-            if (System.IO.File.Exists(file))
-            {
-                log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(file));
-            }
         }
     }
 }
