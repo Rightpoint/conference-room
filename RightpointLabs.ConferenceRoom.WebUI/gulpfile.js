ee96e627a245d6126ef1acaf3b339451031d678a
Jonathan Rupp <jorupp@gmail.com>
2015-05-16 18:24:29 -0500

    Add v1 of release build
    Minimal-files, but not minified

--- RightpointLabs.ConferenceRoom.WebUI/gulpfile.js
+++ RightpointLabs.ConferenceRoom.WebUI/gulpfile.js
@@ -21,6 +21,10 @@ var debug = require('gulp-debug');
 var proxy = require('proxy-middleware');
 var url = require('url');
 var es = require('event-stream');
+var ngTemplates = require('gulp-angular-templatecache');
+var rev = require("gulp-rev");
+var rimraf = require('rimraf');
+var concat = require('gulp-concat');
 
 var JS_SCRIPT_SOURCE = 'src/**/*.js';
 var TS_SCRIPT_SOURCE = 'src/**/*.ts';
@@ -34,30 +38,64 @@ var tsProject = ts.createProject({
     noExternalResolve: true,
     sortProject: true
 });
-gulp.task('scripts', [], function() {
-    // TODO: figure out why having no ts files makes gulp crash with no errors
+
+function scripts() {
     var tsResult = gulp.src(TS_SCRIPT_SOURCE)
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject, null, ts.reporter.longReporter()));
+    return [
+        tsResult.dts,
+        merge([
+            tsResult.js
+                .pipe(sourcemaps.write()),
+            gulp.src(JS_SCRIPT_SOURCE)
+                .pipe(plumber())
+        ])
+    ];
+}
+
+gulp.task('scripts', [], function() {
+    var r = scripts();
     return merge([
-        tsResult.dts
+        r[0]
             .pipe(gulp.dest('dist/definitions')),
-        tsResult.js
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('dist/scripts')),
-        gulp.src(JS_SCRIPT_SOURCE)
-            .pipe(plumber())
+        r[1]
             .pipe(gulp.dest('dist/scripts'))
     ]);
 });
+gulp.task('scripts-release', [], function() {
+    var r = scripts();
+    return merge([
+        r[0]
+            .pipe(gulp.dest('dist/definitions')),
+        merge([
+            gulp.src(wiredep().js)
+                .pipe(plumber())
+                .pipe(sourcemaps.init()),
+            r[1],
+            gulp.src(TEMPLATES_SOURCE)
+                .pipe(plumber())
+                .pipe(ngTemplates({
+                    module: 'app'
+                }))
+                .pipe(debug()) // don't know why this helps, but it does....
+        ])
+            .pipe(concat('scripts.js'))
+            .pipe(rev())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist/scripts'))
+    ]);
+});
+
 gulp.task('templates', [], function () {
     return gulp.src(TEMPLATES_SOURCE)
         .pipe(plumber())
         .pipe(changed('dist'))
         .pipe(gulp.dest('dist'));
 });
-gulp.task('styles', ['fonts'], function () {
+
+function styles(){
     return gulp.src(STYLE_SOURCE)
         .pipe(plumber())
         .pipe(changed('dist/styles'))
@@ -74,16 +112,22 @@ gulp.task('styles', ['fonts'], function () {
                 }
             }
         }))
-        //.pipe(debug())
-        //.pipe(function() {
-        //    function t(file, cb) {
-        //        console.log(String(file.contents));
-        //        cb(null, file);
-        //    }
-        //    return es.map(t);
-        //}())
-        //.pipe(debug())
-        .pipe(less())
+        .pipe(less());
+}
+
+gulp.task('styles', ['fonts'], function () {
+    return styles()
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/styles'));
+});
+gulp.task('styles-release', ['fonts'], function () {
+    return merge([
+            styles(),
+            gulp.src(wiredep().css)
+                .pipe(plumber())
+        ])
+        .pipe(concat('styles.css'))
+        .pipe(rev())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/styles'));
 });
@@ -101,11 +145,18 @@ gulp.task('index', ['scripts', 'styles', 'templates'], function() {
         .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('index-release', ['scripts-release', 'styles-release'], function() {
+    return gulp.src(INDEX_SOURCE)
+        .pipe(plumber())
+        .pipe(inject(gulp.src(['dist/scripts/**/scripts-*.js', 'dist/styles/**/styles-*.css'], { read: false}), { ignorePath: 'dist' }))
+        .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('server', ['index'], function() {
     connect.server({ 
         livereload: { port: 8785 },
         port: PORT, 
-        root: ['dist', '.'], 
+        root: ['dist', '.'],
         middleware: function(c, opt) {
             return [
                 c().use('/bower_components', c.static('./bower_components')),
@@ -114,9 +165,26 @@ gulp.task('server', ['index'], function() {
         }
     });
 });
+gulp.task('server-release', ['index-release'], function() {
+    connect.server({
+        port: PORT,
+        root: 'dist',
+        middleware: function(c, opt) {
+            return [
+                c().use('/api', proxy(url.parse('http://localhost:63915/api')))
+            ];
+        }
+    });
+});
+
 gulp.task('open', ['server'], function() {
     open('http://localhost:' + PORT);
 });
+
+gulp.task('open-release', ['server-release'], function() {
+    open('http://localhost:' + PORT);
+});
+
 gulp.task('reload', [], function() {
     // not working, don't know why
     return gulp.src('dist/index.html').pipe(connect.reload())
@@ -129,6 +197,13 @@ gulp.task('watch', ['scripts'], function() {
     gulp.watch(INDEX_SOURCE, function() { runSequence('index', 'reload'); });
 });
 
+gulp.task('clean', function(callback) {
+    rimraf('dist', callback);
+});
+
 gulp.task('default', function(callback) {
-    return runSequence('open', 'watch', callback);
+    return runSequence('clean', 'open', 'watch', callback);
+});
+gulp.task('release', function(callback) {
+    return runSequence('clean', 'open-release', callback);
 });
