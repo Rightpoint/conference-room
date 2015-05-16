b56bdbb7453f3fbacb64f6405d2c85719b94c111
Jonathan Rupp <jorupp@gmail.com>
2015-05-16 11:11:41 -0500

    Basic rendering of the timeline

--- RightpointLabs.ConferenceRoom.WebUI/src/room/roomController.js
+++ RightpointLabs.ConferenceRoom.WebUI/src/room/roomController.js
@@ -21,6 +21,67 @@
             return moment().add(-timeDelta, 'milliseconds');
         };
 
+        function updateTimeline() {
+            var now = self.currentTime();
+            var day = now.clone().startOf('day');
+            var start = day.clone().add(7, 'hours');
+            var end = day.clone().add(18, 'hours');
+            var totalMinutes = end.diff(start, 'minute', true);
+
+            var marker = start;
+            var markers = [];
+            while(!marker.isAfter(end)) {
+                markers.push({ position: marker.diff(start, 'minute', true) / totalMinutes, time: marker });
+                marker = marker.clone().add(1, 'hour');
+            }
+
+            function buildTimelineRanges() {
+                if(!self.appointments) {
+                    return [{
+                        size: 1,
+                        isLoading: true
+                    }];
+                }
+                var ranges = [];
+                var current = start;
+                _.each(self.appointments, function(item) {
+                    var itemStart = moment(item.Start);
+                    var itemEnd = moment(item.End);
+                    if(!itemStart.isBefore(end) || !itemEnd.isAfter(current)){
+                        return;
+                    }
+                    var freeTime = itemStart.diff(current, 'minute', true);
+                    if(freeTime > 0) {
+                        ranges.push({
+                            size: freeTime/totalMinutes
+                        });
+                    }
+                    var time = moment.min(itemEnd, end).diff(itemStart, 'minute', true);
+                    if(time > 0) {
+                        ranges.push({
+                            size: time/totalMinutes,
+                            appointment: item
+                        })
+                    }
+
+                    current = itemEnd;
+                });
+
+                if(current.isBefore(end)) {
+                    ranges.push({
+                        size: end.diff(current, 'minute', true)/totalMinutes
+                    });
+                }
+
+                return ranges;
+            }
+
+            self.timeline = {
+                now: { position: Math.min(1.05, Math.max(-0.05, now.diff(start, 'minute', true) / totalMinutes)), time: now },
+                markers: markers,
+                ranges: buildTimelineRanges()
+            };
+        }
 
         function loadInfo() {
             self.loadsInProgress++;
@@ -28,11 +89,8 @@
                 self.loadsInProgress--;
 
                 timeDelta = moment().diff(moment(data.CurrentTime));
-
-                console.log(timeDelta, data.CurrentTime);
-                console.log(moment().add(-timeDelta, 'milliseconds'));
-
                 self.displayName = data.DisplayName;
+                updateTimeline();
             }, function() {
                 self.loadsInProgress--;
             });
@@ -52,6 +110,7 @@
                 self.appointments = _.sortBy(data.NearTermMeetings, 'Start');
                 self.current = data.CurrentMeeting;
                 self.next = data.NextMeeting;
+                updateTimeline();
 
                 var waitTime = data.NextChangeSeconds ? Math.min(5 * 60, data.NextChangeSeconds + 1) : (5 * 60);
                 statusTimeout = $timeout(loadStatus, waitTime * 1000);
