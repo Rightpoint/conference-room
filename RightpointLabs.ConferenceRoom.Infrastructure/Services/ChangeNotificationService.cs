32d891ec73fbbcad45e716b6dd002d1fbe42b56a
Jonathan Rupp <jorupp@gmail.com>
2015-05-22 08:13:36 -0500

    v1 of change notification

---
+++ RightpointLabs.ConferenceRoom.Infrastructure/Services/ChangeNotificationService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using log4net;
+using Microsoft.Exchange.WebServices.Data;
+using RightpointLabs.ConferenceRoom.Domain;
+using RightpointLabs.ConferenceRoom.Domain.Services;
+using Timer = System.Timers.Timer;
+
+namespace RightpointLabs.ConferenceRoom.Infrastructure.Services
+{
+    public class ChangeNotificationService : IChangeNotificationService
+    {
+        private static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly ExchangeService _exchangeService;
+        private readonly IBroadcastService _broadcastService;
+
+        private Timer _startConnectionTimer = new Timer(10000);
+        public ChangeNotificationService(ExchangeService exchangeService, IBroadcastService broadcastService)
+        {
+            _exchangeService = exchangeService;
+            _broadcastService = broadcastService;
+            _startConnectionTimer.Stop();
+            _startConnectionTimer.Elapsed += StartConnectionTimerOnElapsed;
+        }
+
+        private void StartConnectionTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            UpdateConnection();
+            _startConnectionTimer.Stop();
+        }
+
+        private StreamingSubscriptionConnection StartNewConnection()
+        {
+            var subIdToRoomAddress = new Dictionary<string, string>();
+            var subs = new List<StreamingSubscription>();
+            foreach (var room in _roomsTracked)
+            {
+                var calId = new FolderId(WellKnownFolderName.Calendar, new Mailbox(room));
+                var sub = _exchangeService.SubscribeToStreamingNotifications(
+                    new [] { calId },
+                    EventType.Created,
+                    EventType.Deleted,
+                    EventType.Modified,
+                    EventType.Moved,
+                    EventType.Copied,
+                    EventType.FreeBusyChanged);
+                subIdToRoomAddress.Add(sub.Id, room);
+                subs.Add(sub);
+            }
+
+            // Create a streaming connection to the service object, over which events are returned to the client.
+            // Keep the streaming connection open for 30 minutes.
+            var connection = new StreamingSubscriptionConnection(_exchangeService, 30);
+            subs.ForEach(connection.AddSubscription);
+            connection.OnNotificationEvent += OnNotificationEvent;
+            connection.OnDisconnect += OnDisconnect;
+            connection.Open();
+            _subIdToRoomAddress = subIdToRoomAddress;
+            return connection;
+        }
+
+        private void OnDisconnect(object sender, SubscriptionErrorEventArgs args)
+        {
+            UpdateConnection();
+        }
+
+        private void OnNotificationEvent(object sender, NotificationEventArgs args)
+        {
+            var d = _subIdToRoomAddress;
+            if (null != d)
+            {
+                var room = d.TryGetValue(args.Subscription.Id);
+                if (!string.IsNullOrEmpty(room))
+                {
+                    _broadcastService.BroadcastUpdate(room);
+                }
+            }
+        }
+
+        private object _connectionLock = new object();
+        private void UpdateConnection()
+        {
+            try
+            {
+                lock (_connectionLock)
+                {
+                    if (null != _connection)
+                    {
+                        if (_connection.IsOpen)
+                        {
+                            _connection.Close();
+                        }
+                    }
+                    _connection = StartNewConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                log.WarnFormat("Error setting up connection: {0}", ex);
+                _startConnectionTimer.Start(); // retry
+            }
+        }
+
+        private Dictionary<string, string> _subIdToRoomAddress = new Dictionary<string, string>();
+        private HashSet<string> _roomsTracked = new HashSet<string>();
+        private StreamingSubscriptionConnection _connection;
+
+        public void TrackRoom(string roomAddress)
+        {
+            if (_roomsTracked.Contains(roomAddress)) 
+                return;
+            _roomsTracked.Add(roomAddress);
+            UpdateConnection();
+        }
+    }
+}
