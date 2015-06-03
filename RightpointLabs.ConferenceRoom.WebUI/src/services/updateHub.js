db3aae9e439ea534d1c71ac858c73517515e22b9
Jonathan Rupp <jorupp@gmail.com>
2015-06-03 18:06:59 -0500

    don't notify for initial connection

--- RightpointLabs.ConferenceRoom.WebUI/src/services/updateHub.js
+++ RightpointLabs.ConferenceRoom.WebUI/src/services/updateHub.js
@@ -2,6 +2,7 @@
     'use strict';
 
     angular.module('app').factory('UpdateHub', function($rootScope, Hub, logger){
+        var isFirstEvent = true;
         var hub = new Hub('UpdateHub', {
             listeners: {
                 'Update': function (room) {
@@ -11,14 +12,20 @@
                 }
             },
             errorHandler: function (error) {
+                isFirstEvent = false;
                 logger.error(error);
             },
             stateChanged: function(state) {
                 switch(state.newState) {
                     case $.signalR.connectionState.connected:
-                        logger.success('Live updates connected');
+                        if(!isFirstEvent) {
+                            // no need to tell the user about the first event if it's a connection - we just want to tell them about future ones
+                            logger.success('Live updates connected');
+                        }
+                        isFirstEvent = false;
                         break;
                     case $.signalR.connectionState.disconnected:
+                        isFirstEvent = false;
                         logger.warn('Live updates suspended');
                         break;
                 }
