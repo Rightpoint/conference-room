32445f3d4d7e0354481d804a8be6186cb6492819
Jonathan Rupp <jrupp@rightpoint.com>
2015-06-28 07:09:01 +0800

    switch to BBB-compat library and get working there

--- RightpointLabs.ConferenceRoom.StatusMonitor/index.js
+++ RightpointLabs.ConferenceRoom.StatusMonitor/index.js
@@ -2,29 +2,41 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var Promise = require('promise');
-var gpio = require('rpi-gpio');
+var Gpio = require('onoff').Gpio;
 var async = require('async');
 var signalR = require('signalr-client');
 var path = require('path');
+var Gpio = require('onoff').Gpio,
+	sleep = require('sleep');
 
 var configFile = path.join(__dirname, 'config.json');
 console.log('Loading configuration from ' + configFile);
 var config = JSON.parse(fs.readFileSync(configFile));
 
-async.parallel([
-    function(c) {
-        gpio.setup(config.redPin, gpio.DIR_OUT, c);
-    },
-    function(c) {
-        gpio.setup(config.greenPin, gpio.DIR_OUT, c);
-    }
-], function (e, results) {
-    console.log('gpio setup');
-    console.log(e);
-    console.log(results);
-    setPins(false, false);
-    updateIn(1);
-});
+var redLed = new Gpio(config.redPin, 'out');
+var greenLed = new Gpio(config.greenPin, 'out');
+var speakerPin = new Gpio(config.speakerPin, 'out');
+
+setPins(false, false, false);
+doBeep(speakerPin, 1046, 0.04); // High C
+sleep.usleep(1000000 * 0.01);
+doBeep(speakerPin, 1046, 0.04); // High C
+sleep.usleep(1000000 * 0.01);
+doBeep(speakerPin, 1318.51, 0.08); // E
+sleep.usleep(1000000 * 0.01);
+doBeep(speakerPin, 1318.51, 0.08); // E
+
+// test red
+setPins(true, false, false);
+sleep.usleep(1000000 * 0.3);
+
+// test green
+setPins(false, true, false);
+sleep.usleep(1000000 * 0.3);
+
+// and start
+setPins(false, false, false);
+updateIn(1);
 
 function getStatus() {
     return new Promise(function(resolve, reject) {
@@ -70,25 +82,33 @@ function updateIn(delay) {
 
 function red() {
     console.log('red');
-    setPins(true, false);
+    setPins(true, false, true);
 }
 function green() {
     console.log('green');
-    setPins(false, true);
+    setPins(false, true, true);
 }
-function setPins(red, green) {
-    async.parallel([
-        function(c) {
-            gpio.write(config.redPin, red, c);
-        },
-        function(c) {
-            gpio.write(config.greenPin, green, c);
-        }
-    ], function (e, results) {
-        console.log('set pins');
-        console.log(e);
-        console.log(results);
-    });
+function doBeep(pin, hz, duration) {
+	var d = Math.floor( 1000000 / hz);
+	var c = Math.floor((duration * hz) / 2);
+	for(var i=0; i<c; i++) {
+		pin.writeSync(0);
+		sleep.usleep( d );
+		pin.writeSync(1);
+		sleep.usleep( d );
+	}	
+}
+function setPins(red, green, canBeep) {
+	var beep = canBeep && (redLed.readSync() != red || greenLed.readSync() != green);
+	redLed.writeSync(red ? 1 : 0);
+	greenLed.writeSync(green ? 1 : 0);
+	console.log('set pins');
+	if(beep)
+	{
+		doBeep(speakerPin, 1046, 0.04); // High C
+		sleep.usleep(1000000 * 0.01);
+		doBeep(speakerPin, 1318.51, 0.08); // E
+	}
 }
 
 setInterval(function() {
