fcb627c38b03690fc37712df0a2b40b6eb95416e
Jonathan Rupp <jorupp@gmail.com>
2015-08-05 19:21:59 -0500

    Progress towards a lock protecting the settings screen

--- RightpointLabs.ConferenceRoom.WebUI/src/settings/settingsController.js
+++ RightpointLabs.ConferenceRoom.WebUI/src/settings/settingsController.js
@@ -1,9 +1,18 @@
 (function() {
     'use strict;'
 
-    angular.module('app').controller('SettingsController', ['settings', function(settings) {
+    angular.module('app').controller('SettingsController', ['settings', '$modal', function(settings, $modal) {
         var self = this;
         
+        self.isLocked = true;
         self.settings = settings;
+        
+        self.unlock = function () {
+            $modal.open({
+                templateUrl: 'settings/modal.html'
+            }).result.then(function (data) {
+                window.alert(data);
+            })
+        };
     }]);
 })();
