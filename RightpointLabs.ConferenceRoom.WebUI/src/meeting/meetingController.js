19841223e3f51d02fc8fe26eb63b553bde5ba2c5
Jonathan Rupp <jrupp@rightpoint.com>
2016-03-11 09:14:21 -0600

    Show a better message for multi-day meetings

--- RightpointLabs.ConferenceRoom.WebUI/src/meeting/meetingController.js
+++ RightpointLabs.ConferenceRoom.WebUI/src/meeting/meetingController.js
@@ -4,8 +4,12 @@
     angular.module('app').controller('MeetingController', ['meeting', function(meeting) {
         var self = this;
         self.meeting = meeting;
+        
+        var isSameDay = moment(meeting.Start).startOf('day').isSame(moment(meeting.End).startOf('day'));
+        var format = isSameDay ? 'h:mm a' : 'MMMM Do YYYY, h:mm a';
+        
         self.formatTime = function formatTime(value) {
-            return moment(value).format('h:mm a');
+            return moment(value).format(format);
         };
     }]);
 })();
