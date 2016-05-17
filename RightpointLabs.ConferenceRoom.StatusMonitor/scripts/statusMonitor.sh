f814b6eb033615426f7c792ba80145d3d29236ee
Jonathan Rupp <jrupp@rightpoint.com>
2016-05-17 16:43:38 -0500

    restart pi-blaster when launching the status app so fades are reliable

--- RightpointLabs.ConferenceRoom.StatusMonitor/scripts/statusMonitor.sh
+++ RightpointLabs.ConferenceRoom.StatusMonitor/scripts/statusMonitor.sh
@@ -4,6 +4,7 @@ sleep 3 # we're starting too early
 
 while true
 do
+	/bin/systemctl restart pi-blaster.service
         /usr/bin/node ~pi/statusMonitor/index.js 2>&1 1>>~pi/statusMonitor.log
         sleep 1
 done
