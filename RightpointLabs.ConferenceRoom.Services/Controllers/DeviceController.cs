cc807bf72d75819a5cbbd523dadc15b9a0d6fd83
Jonathan Rupp <jrupp@rightpoint.com>
2016-09-27 19:15:48 -0500

    Beginning of token-request controllers

---
+++ RightpointLabs.ConferenceRoom.Services/Controllers/DeviceController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RightpointLabs.ConferenceRoom.Domain.Models;
+using RightpointLabs.ConferenceRoom.Domain.Models.Entities;
+using RightpointLabs.ConferenceRoom.Domain.Repositories;
+
+namespace RightpointLabs.ConferenceRoom.Services.Controllers
+{
+    [RoutePrefix("api/devices")]
+    public class DeviceController : ApiController
+    {
+        private readonly IOrganizationRepository _organizationRepository;
+        private readonly IDeviceRepository _deviceRepository;
+
+        public DeviceController(IOrganizationRepository organizationRepository, IDeviceRepository deviceRepository)
+        {
+            _organizationRepository = organizationRepository;
+            _deviceRepository = deviceRepository;
+        }
+
+        [Route("create")]
+        public object PostCreate(string organizationId, string joinKey)
+        {
+            var org = _organizationRepository.Get(organizationId);
+
+            if (org?.JoinKey != joinKey)
+            {
+                return new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+
+            var device = _deviceRepository.Create(new DeviceEntity()
+            {
+                OrganizationId = org.Id
+            });
+
+            var token = CreateToken(device);
+
+            return token;
+        }
+
+        private string CreateToken(DeviceEntity device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RightpointLabs.ConferenceRoom.Services/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RightpointLabs.ConferenceRoom.Domain.Models;
+using RightpointLabs.ConferenceRoom.Domain.Models.Entities;
+using RightpointLabs.ConferenceRoom.Domain.Repositories;
+
+namespace RightpointLabs.ConferenceRoom.Services.Controllers
+{
+    [RoutePrefix("api/tokens")]
+    public class TokenController : ApiController
+    {
+        [Route("get")]
+        public object PostGet()
+        {
+            var token = CreateToken(this.Request);
+            if (null == token)
+            {
+                return StatusCode(HttpStatusCode.Forbidden);
+            }
+            return token;
+        }
+
+        private string CreateToken(HttpRequestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
