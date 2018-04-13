22a35b55d29ecacc06d7992022e079bc294e7e3b
Jonathan Rupp <jrupp@rightpoint.com>
2018-04-13 13:28:53 -0500

    Fix swagger UI buildout

--- Web/App_Start/SwaggerConfig.cs
+++ Web/App_Start/SwaggerConfig.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using System.Web.Http;
 using WebActivatorEx;
 using RightpointLabs.ConferenceRoom.Web;
 using Swashbuckle.Application;
+using Swashbuckle.Swagger;
+using System.Web.Http.Description;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -17,15 +20,33 @@ namespace RightpointLabs.ConferenceRoom.Web
                 .EnableSwagger(c =>
                     {
                         c.SingleApiVersion("v2", "RightpointLabs.ConferenceRoom.Web");
-                        c.ApiKey("token")
+                        c.ApiKey("api_key")
                             .Description("Bearer token here")
                             .Name("Authorization")
                             .In("header");
+                        c.OperationFilter<AssignSecurityRequirements>();
                     })
                 .EnableSwaggerUi(c =>
                 {
+                    c.DocumentTitle("Room Ninja API");
                     c.EnableApiKeySupport("Authorization", "header");
                 });
+        }
+
+        public class AssignSecurityRequirements : IOperationFilter
+        {
+            public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+            {
+                operation.security = new List<IDictionary<string, IEnumerable<string>>>()
+                {
+                    new Dictionary<string, IEnumerable<string>>()
+                    {
+                        {
+                            "api_key", new string[0]
+                        }
+                    }
+                };
+            }
         }
     }
 }
