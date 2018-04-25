1ee6d6f8adbe0d6be82e90510ed0dc8b97ddc243
Jonathan Rupp <jorupp@gmail.com>
2018-04-24 19:28:10 -0500

    control the whole URL from config

--- Bot/Controllers/IndexController.cs
+++ Bot/Controllers/IndexController.cs
@@ -16,7 +16,7 @@ namespace RightpointLabs.ConferenceRoom.Bot.Controllers
         [HttpGet]
         public async Task<object> Get()
         {
-            return Content($"<html><head><title>Room Ninja Bot</title></head><body style='margin: 0; padding: 0;'><iframe style='height: 100%; width: 100%; margin: 0; padding: 0; border: 0;' src='https://webchat.botframework.com/embed/rproomsbot2?s={Config.GetAppSetting("BOT_WEB_SECRET")}'></iframe></body></html>", "text/html");
+            return Content($"<html><head><title>Room Ninja Bot</title></head><body style='margin: 0; padding: 0;'><iframe style='height: 100%; width: 100%; margin: 0; padding: 0; border: 0;' src='{Config.GetAppSetting("BOT_WEB_URL")}'></iframe></body></html>", "text/html");
         }
     }
 }
