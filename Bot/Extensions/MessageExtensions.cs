d558c40721ce92ca51f550abd725ff0501a18fac
Jonathan Rupp <jrupp@rightpoint.com>
2018-06-20 20:15:51 -0500

    Try to direct responses to threads, but doesn't actually work...

--- Bot/Extensions/MessageExtensions.cs
+++ Bot/Extensions/MessageExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json.Linq;
 
 namespace RightpointLabs.ConferenceRoom.Bot.Extensions
 {
@@ -8,10 +9,31 @@ namespace RightpointLabs.ConferenceRoom.Bot.Extensions
         public static IMessageActivity CreateMessage(this IDialogContext context, string text, string speak, string inputHint)
         {
             var msg = context.MakeMessage();
+
+            if (context.Activity.ChannelId == "slack")
+            {
+                // let's try to respond to the thread....
+                var channelData = GetActivityChannelData(context, new {SlackMessage = new {@event = new {ts = "", thread_ts = ""}}});
+                var ts = channelData?.SlackMessage?.@event?.thread_ts;
+                if (string.IsNullOrEmpty(ts))
+                {
+                    ts = channelData?.SlackMessage?.@event?.ts;
+                }
+                if (!string.IsNullOrEmpty(ts))
+                {
+                    msg.ChannelData = JObject.FromObject(new {thread_ts = ts});
+                }
+            }
+
             msg.Text = text;
             msg.Speak = speak;
             msg.InputHint = inputHint;
             return msg;
+        }
+        
+        private static T GetActivityChannelData<T>(IDialogContext context, T prototype)
+        {
+            return context.Activity.GetChannelData<T>();
         }
 
         public static IMessageActivity CreateMessage(this IDialogContext context, string text, string inputHint)
