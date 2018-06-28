27da37177d6d6499d1ebf718576d416a1da8fbcc
Jonathan Rupp <jrupp@rightpoint.com>
2018-06-27 20:54:32 -0500

    Start a new thread for each message on slack

--- Bot/Extensions/MessageExtensions.cs
+++ Bot/Extensions/MessageExtensions.cs
@@ -11,21 +11,6 @@ namespace RightpointLabs.ConferenceRoom.Bot.Extensions
         {
             var msg = context.MakeMessage();
 
-            if (context.Activity.ChannelId == "slack")
-            {
-                // let's try to respond to the thread....
-                var channelData = GetActivityChannelData(context, new {SlackMessage = new {@event = new {ts = "", thread_ts = ""}}});
-                var ts = channelData?.SlackMessage?.@event?.thread_ts;
-                if (string.IsNullOrEmpty(ts))
-                {
-                    ts = channelData?.SlackMessage?.@event?.ts;
-                }
-                if (!string.IsNullOrEmpty(ts))
-                {
-                    msg.ChannelData = JObject.FromObject(new {thread_ts = ts});
-                }
-            }
-
             msg.Text = text;
             msg.Speak = speak;
             msg.InputHint = inputHint;
